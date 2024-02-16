5a31eb64109fc13797d29cc41953370abc6bfdad
sciarbel <sciarbel@gmail.com>
2024-02-16 00:01:26 -0300

    Added second test to check if file already exists, and a exit 0 at the end of the script.

--- bashland/scriptmake.sh
+++ bashland/scriptmake.sh
@@ -5,7 +5,11 @@
 # File           : scriptmake.sh
 # Description    : A simple shell script that simple shell script that takes a filename as a command line argument ($1). It creates a new file with the provided name, adds a header with some information, and makes the file executable. Additionally, it opens the file in the nano text editor for editing.
 
-[[ $# -ne 1 ]] && echo 'Type a valid filename, please' && exit 1
+#Check if the file name is provided as a command line argument
+[[ $# -ne 1 ]] && echo 'Type a valid file name, please' && exit 1
+
+#Check if the file already exists
+[[ -f "$1" ]] && echo 'File already exists. Exiting.' && exit 1
 
 echo '#!/usr/bin/env bash' >"$1"
 
@@ -15,6 +19,8 @@ Filename : '$1'" >> "$1"
 
 chmod +x "$1"
 nano "$1"
+
+exit 0
 
 #Parameters:
 # <filename>: The name of the file to be created and edited.
