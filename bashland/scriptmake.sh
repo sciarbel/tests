ce80b59dde7354c20fc64d3391ffa5b1ee6263c5
sciarbel <sciarbel@gmail.com>
2024-02-15 23:46:02 -0300

    Corrected some errors in documentation

--- bashland/scriptmake.sh
+++ bashland/scriptmake.sh
@@ -3,19 +3,19 @@
 # Author         : test 
 # Date           : Wed 31 Feb 2024 23:38:50 IST
 # File           : scriptmake.sh
-# Description    : A simple shell script that takes a filename as a command line argument ($1), creates a new file with the given name, makes it executable, and opens it in the nano text editor for editing.
+# Description    : A simple shell script that simple shell script that takes a filename as a command line argument ($1). It creates a new file with the provided name, adds a header with some information, and makes the file executable. Additionally, it opens the file in the nano text editor for editing.
 
 [[ $# -ne 1 ]] && echo 'Type a valid filename, please' && exit 1
 
 echo '#!/usr/bin/env bash' >"$1"
+
 date >> "$1"
 echo "Author   :  test
 Filename : '$1'" >> "$1"
+
 chmod +x "$1"
 nano "$1"
 
-#Usage: ./scriptmake.sh <filename>
 #Parameters:
-#- filename: The name of the file to be created and edited.
-#Example:
-#./scriptmake.sh new_script
+# <filename>: The name of the file to be created and edited.
+#Usage: ./scriptmake.sh <filename>
