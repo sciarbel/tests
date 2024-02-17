35008e89b8deca7327abbd6d76a8b75e347728c1
sciarbel <sciarbel@gmail.com>
2024-02-16 21:08:30 -0300

    Added header variable, messed with documentation

--- bashland/scriptmake.sh
+++ bashland/scriptmake.sh
@@ -1,27 +1,33 @@
 #!/usr/bin/env bash
+#--------------------------------------------
+# Author     : test 
+# Date       : Wed 31 Feb 2024 23:38:50 IST
+# File       : scriptmake.sh
+#--------------------------------------------
+# Description: A simple shell script that simple shell script that takes a filename as a command line argument ($1). It creates a new file with the provided name, adds a header with some information, and makes the file executable. Additionally, it opens the file in the nano text editor for editing.
 
-# Author         : test 
-# Date           : Wed 31 Feb 2024 23:38:50 IST
-# File           : scriptmake.sh
-# Description    : A simple shell script that simple shell script that takes a filename as a command line argument ($1). It creates a new file with the provided name, adds a header with some information, and makes the file executable. Additionally, it opens the file in the nano text editor for editing.
+# Usage: ./scriptmake.sh <filename>
 
-#Check if the file name is provided as a command line argument
+# Check if the file name is provided as a command line argument
 [[ $# -ne 1 ]] && echo 'Type a valid file name, please' && exit 1
 
-#Check if the file already exists
+# Check if the file already exists
 [[ -f "$1" ]] && echo 'File already exists. Exiting.' && exit 1
 
-echo '#!/usr/bin/env bash' >"$1"
+# Header
+header="#!/usr/bin/env bash
+#--------------------------------------------
+# Date     : $(date)
+# Author   : test
+# File name: $1
+#--------------------------------------------
+# Usage    :
+#
+"
 
-date >> "$1"
-echo "Author   :  test
-Filename : '$1'" >> "$1"
+echo "$header" >> "$1"
 
 chmod +x "$1"
 nano "$1"
 
 exit 0
-
-#Parameters:
-# <filename>: The name of the file to be created and edited.
-#Usage: ./scriptmake.sh <filename>
